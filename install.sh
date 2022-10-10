a288aa7788c76abb7e52c56d68e441d4808b61eb
jim <jim@vheavy.com>
2022-10-10 12:16:35 -0700

    Add install.sh, fix src/Makefile

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt-get install openjdk-11-jdk-headless 
+
+make -C joystick/linux
+cp joystick/lib/*.so pplib/
+
+make -C src libFrameCaptureJNI.so
+cp src/libFrameCaptureJNI.so pplib/
+
+make -C src priuspilot.jar
+cp src/priuspilot.jar pplib/
+
+
