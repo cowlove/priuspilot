eb1585f7fe7e8a760196da35e3a1c94238d04304
jim <jim@vheavy.com>
2022-10-11 11:06:42 -0700

    Fix test.sh

--- test.sh
+++ test.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-rm /tmp/test.out
+TMP=/tmp/test.out
+rm $TMP
 FLAGS="-debug SHOW_FPS -displayratio 0"
 
 time(
-./scripts/pp $FLAGS lanedumps/20221010.151339.yuv -skip 1900 -exit 500   | tee -a /tmp/test.out 
-./scripts/pp $FLAGS lanedumps/20221010.145903.yuv -skip 2400 -exit 800 | tee -a /tmp/test/out
+./scripts/pp $FLAGS lanedumps/20221010.151339.yuv -skip 1900 -exit 500   | tee -a $TMP 
+./scripts/pp $FLAGS lanedumps/20221010.145903.yuv -skip 2400 -exit 800 | tee -a $TMP
 
-for f in lanedumps/*.yuv; do ./scripts/pp $FLAGS $f ; done | tee -a /tmp/test.out
+for f in lanedumps/*.yuv; do ./scripts/pp $FLAGS $f ; done | tee -a $TMP
 )
 
-grep RMS /tmp/test.out | sort > test.out
+grep RMS $TMP | sort > test.out
 cat test.out
