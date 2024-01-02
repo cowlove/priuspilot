aab45055c5d558279e2f85ccc5a0a450235ff53e
Jim Evans <jim@vheavy.com>
2024-01-02 05:39:36 -0800

    Clean up install script

--- install.sh
+++ install.sh
@@ -1,26 +1,22 @@
 #!/bin/bash -x 
 
+sudo apt-get -y install \
+    git python3.9-distutils \
+    openjdk-17-jdk-headless openjdk-17-jdk xfce4-terminal automake \
+    tightvncserver scons
+
 mkdir ~/src
 cd !$
 git clone https://github.com/cowlove/priuspilot.git
-
-cd ~/src/priuspilot
-mkdir lanedumps
+mkdir ~/src/priuspilot/lanedumps
 
 sudo mkdir -p /host/lanedumps
 sudo chown pi:pi !$
 
-
-sudo apt-get -y install openjdk-17-jdk-headless openjdk-17-jdk xfce4-terminal automake \
-    tightvncserver syslog-ng scons
-
 cd ~/src
 git clone https://gitlab.com/gpsd/gpsd.git
-cd cd gpsd
+cd gpsd
 scons
-
-
-
 
 cd ~/src
 git clone https://github.com/kernc/logkeys.git
@@ -30,7 +26,6 @@ cd build
 ../configure
 make
 sudo make install
-
 
 cd ~/src/priuspilot
 make -C joystick/linux
@@ -43,5 +38,5 @@ make -C src priuspilot.jar
 cp src/priuspilot.jar pplib/
 
 #test installation
-./scripts/pp 
+#./scripts/pp 
  
