58baac9cb8adfe2a78413c0e522a8be2915f7c0d
Jim Evans <jim@vheavy.com>
2024-01-10 22:42:51 -0800

    re-enable microkey

--- scripts/microkey.sh
+++ scripts/microkey.sh
@@ -5,14 +5,14 @@ while sleep 1; do
         sleep 1
         stdbuf -o0 evtest /dev/input/event3 | tee -a /tmp/keys  
     done
-    echo power off | bluetoothctl
-    sleep 1
-    echo power on | bluetoothctl
-    sleep 3
-    echo agent on | bluetoothctl
-    echo scan on | bluetoothctl
-    sleep 3
-    echo pair E4:17:D8:14:F2:BE | bluetoothctl
+    #echo power off | bluetoothctl
+    #sleep 1
+    #echo power on | bluetoothctl
+    #sleep 3
+    #echo agent on | bluetoothctl
+    #echo scan on | bluetoothctl
+    #sleep 3
+    #echo pair E4:17:D8:14:F2:BE | bluetoothctl
     echo connect E4:17:D8:14:F2:BE | bluetoothctl
     sleep 1
 done
