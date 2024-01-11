4d6cd4b9cc783b9cefde06818c3f38d875127a80
Jim Evans <jim@vheavy.com>
2024-01-10 22:10:04 -0800

    More bluetooth init steps for microkeys.sh

--- scripts/microkey.sh
+++ scripts/microkey.sh
@@ -8,8 +8,10 @@ while sleep 1; do
     echo power off | bluetoothctl
     sleep 1
     echo power on | bluetoothctl
-    sleep 2
+    sleep 3
     echo agent on | bluetoothctl
+    echo scan on | bluetoothctl
+    sleep 3
     echo pair E4:17:D8:14:F2:BE | bluetoothctl
     echo connect E4:17:D8:14:F2:BE | bluetoothctl
     sleep 1
