82d75c39b9f0fa41f6c379f9bfe7f516901c105f
Jim Evans <jim@vheavy.com>
2024-01-11 08:55:55 -0800

    Multiple keyboard /tmp/key servers

--- scripts/microkey.sh
+++ scripts/microkey.sh
@@ -3,7 +3,7 @@
 while sleep 1; do
     for f in { 1 .. 3 }; do 
         sleep 1
-        stdbuf -o0 evtest /dev/input/event4 | tee -a /tmp/keys  
+        stdbuf -o0 evtest $1 | tee -a /tmp/keys  
     done
     #echo power off | bluetoothctl
     #sleep 1
@@ -13,7 +13,9 @@ while sleep 1; do
     #echo scan on | bluetoothctl
     #sleep 3
     #echo pair E4:17:D8:14:F2:BE | bluetoothctl
-    echo connect E4:17:D8:14:F2:BE | bluetoothctl
+    echo connect E4:17:D8:14:F2:BE | bluetoothctl # 8bit 
+    sleep 1
+    echo connect D0:54:7B:88:41:36 | bluetoothctl # mocute
     sleep 1
 done
 
