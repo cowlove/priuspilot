ad6f50e9e8d1ed971ae98f86826ebf7eda720fc7
Jim Evans <jim@vheavy.com>
2024-02-08 13:33:13 -0800

    tuning

--- src/SteeringLogic.java
+++ src/SteeringLogic.java
@@ -8,10 +8,10 @@
 class SteeringLogicSimpleLimits {
 	double maxSteer =  0.50;
 	double maxChange = 0.0048; // per ms
-	double deadband = 0.20;
+	double deadband = 0.35;
 	double curveGain = 0.00;
 	double speedGain = 0.00;
-	double finalGain = .40;
+	double finalGain = .43;
 	double asymDetune = -1.00;
 	double trim = -0.0;
 
