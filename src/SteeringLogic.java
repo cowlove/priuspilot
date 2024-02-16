9ce54b197ae57d390a1852179fa9739b385132a8
Jim Evans <jim@vheavy.com>
2024-02-16 06:29:12 -0800

    Make steering.deadband post-gain value

--- src/SteeringLogic.java
+++ src/SteeringLogic.java
@@ -6,35 +6,38 @@
 
 
 class SteeringLogicSimpleLimits {
-	double maxSteer =  0.60;
-	double maxChange = 0.0048; // per ms
-	double deadband = 0.37;
+	double maxSteer =  0.60; // post-gain
+	double maxChange = 0.0048; // per ms pre-gain
+	double deadband = 0.15; // post-gain
 	double curveGain = 0.00;
 	double speedGain = 0.00;
 	double finalGain = .42;
 	double asymDetune = -1.00;
-	double trim = -0.06;
+	double trim = -0.06; // post-gain
 
 	long lastMs = 0;
 	double lastSteer = 0;
 	double steer(long ms, double st, double curve, double speed) { 
-		if (Double.isNaN(lastSteer)) lastSteer = 0.0;
-		if (deadband > 0 || (deadband < 0 && Math.abs(st) > Math.abs(deadband))) { 
-			if (st < 0) st -= deadband;
-			if (st > 0) st += deadband;
-        }
-
-
-		double maxDelta = maxChange * (ms - lastMs);
-		st = Math.min(lastSteer + maxDelta, Math.max(lastSteer - maxDelta, st));
-		lastMs = ms;
-		lastSteer = st; // pre final gain 
-
 		// gain is set for nominal 60mph.  increase gain for faster speeds, decrease for lower
 		final double maxSpeedMod = 0.20;
-		double speedMod = Math.max(-maxSpeedMod, Math.min(maxSpeedMod, (speed - 60.0) * speedGain));
-		st *= (finalGain + speedMod);
+		final double speedMod = Math.max(-maxSpeedMod, Math.min(maxSpeedMod, (speed - 60.0) * speedGain));
+		final double gain = finalGain + speedMod;
 
+		if (deadband > 0) { 
+			if (st < 0) st -= deadband / gain;  // convert to post-gain value 
+			if (st > 0) st += deadband / gain;
+		}
+
+		double maxDelta = maxChange * (ms - lastMs);
+		if (Double.isNaN(lastSteer)) 
+			lastSteer = 0.0;
+		st = Math.min(lastSteer + maxDelta, Math.max(lastSteer - maxDelta, st));
+		lastMs = ms;
+		totalAction += Math.abs(st - lastSteer);
+		lastSteer = st; // pre final gain 
+
+		st *= gain;
+		
 		// move the maxSteer limit window based on curve 
 		double curveMod = curve * curveGain;
 		st = Math.max(-maxSteer + curveMod, Math.min(maxSteer + curveMod, st));
@@ -44,7 +47,6 @@ class SteeringLogicSimpleLimits {
 		}
 		st += trim;
 
-		totalAction += Math.abs(st - lastSteer);
 		return st;
 	}
 	double totalAction = 0.0;
